d87e785e4ff4c53720683c28865ce2b1f3fd2bb4
Palina <palinabohush@gmail.com>
2026-02-24 16:19:23 +0000

    Added comments to .h

--- src/digit_sum.h
+++ src/digit_sum.h
@@ -1,5 +1,14 @@
+/* Author: Bohush Palina, group 11a */
 #ifndef DIGIT_SUM_H
 #define DIGIT_SUM_H
+
+/*
+ * Function: digit_sum
+ * -------------------
+ * Calculates sum of digits of a number.
+ * number: natural number
+ * returns: sum of digits
+ */
 
 int digit_sum(int number);
 
