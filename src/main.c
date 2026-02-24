7fe5ed59f998da7341ad5469a542849a0c3e3251
Palina <palinabohush@gmail.com>
2026-02-24 16:06:00 +0000

    Added generated array

--- src/main.c
+++ src/main.c
@@ -1,8 +1,25 @@
 /* hello.c */
 #include <stdio.h>
 
-void main (void)
+void main (int argc, char* argv[])
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+	if (argc != 2)
+		{
+			print("Not enough arguements\n");
+			return 1;
+		}
+
+	int n = atoi(argv[1]);
+
+	int array[n];
+
+  srand(time(NULL));
+
+	printf("Generated array:\n");
+
+  for (int i = 0; i < n; i++)
+  {
+    array[i] = rand() % 1000 + 1;
+    printf("%d ", array[i]);
+  }
 }
