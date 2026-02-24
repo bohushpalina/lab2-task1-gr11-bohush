65b5eef0a9da050b0068ee8fa68a86385963d017
Palina <palinabohush@gmail.com>
2026-02-24 16:17:23 +0000

    Updated digit_sum.c

---
+++ src/digit_sum.c
@@ -0,0 +1,12 @@
+#include "digit_sum.h"
+
+int digit_sum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
