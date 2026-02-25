b0401d08b468deca47809ad465c5bb150ceafe2b
Palina <palinabohush@gmail.com>
2026-02-25 15:44:52 +0300

    Update digit_sum.h

--- src/digit_sum.h
+++ src/digit_sum.h
@@ -3,11 +3,32 @@
  * Group:       11a
  */
 
-#ifndef DIGIT_SUM_H
-#define DIGIT_SUM_H
+#ifndef ARRAY_UTILS_H
+#define ARRAY_UTILS_H
+
+void generate_array(
+        int             *array,
+        int             size
+);
+
+void print_array(
+        const int       *array,
+        int             size
+);
+
+int find_max_digit_sum(
+        const int       *array,
+        int             size
+);
+
+void print_numbers_with_max_sum(
+        const int       *array,
+        int             size,
+        int             max_sum
+);
 
 int digit_sum(
         int             number
 );
 
-#endif /* DIGIT_SUM_H */
+#endif /* ARRAY_UTILS_H */
